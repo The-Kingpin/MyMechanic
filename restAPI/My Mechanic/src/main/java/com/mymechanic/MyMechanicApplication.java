023653a14853f332c59f18eb8a76ac6792a8175b
d3v1lkin <svetoslav.ivanov@windowslive.com>
2020-07-15 17:40:31 +0300

    Initial commit back-end

---
+++ restAPI/My Mechanic/src/main/java/com/mymechanic/MyMechanicApplication.java
@@ -0,0 +1,13 @@
+package com.mymechanic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMechanicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMechanicApplication.class, args);
+    }
+
+}
