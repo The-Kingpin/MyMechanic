6fff9ef4214918e0d76247cd7260a588a0f4008c
d3v1lkin <svetoslav.ivanov@windowslive.com>
2020-07-15 18:38:26 +0300

    Add user controller and entity

---
+++ restAPI/My Mechanic/src/main/java/com/mymechanic/entities/User.java
@@ -0,0 +1,30 @@
+package com.mymechanic.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String userName;
+
+
+    private String email;
+
+    private String password;
+
+    public User() {
+    }
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+
+}
